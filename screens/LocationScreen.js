459a9daef254b0bf28c98971a0d049e15c076d7d
Shan <zeshan78938@gmail.com>
2021-09-27 18:02:31 +0500

    Loction Screen designed

--- screens/LocationScreen.js
+++ screens/LocationScreen.js
@@ -1,81 +1,106 @@
-import React from 'react';
-import {StyleSheet, View,Text, StatusBar, Platform} from 'react-native';
-import ListItemSeparator from '../components/ListItemSeparator';
-import Colors from '../config/Colors';
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+  Platform,
+  TouchableWithoutFeedback,
+} from "react-native";
+import ListItemSeparator from "../components/ListItemSeparator";
+import Colors from "../config/Colors";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Fontisto } from "@expo/vector-icons";
+import * as Linking from "expo-linking"
 
 function LocationScreen(props) {
-    return (
-        <View style={styles.container}>
-        <View style={styles.innerContainer}>
-            <View style={styles.OpeningTimings}>
-                <Text style={styles.TextHeading}>OpeningTimings: From 12 am to 6Pm</Text>
-            </View>
-            <View style={styles.PhoneNumber}>
-            <Text style={styles.PhoneHeadingText}>042-83483949384</Text>
-            </View>
-            <View style={styles.Address}><Text style={styles.AddressText}>Food Court,Pakages Mall,LAHORE</Text></View>
+    const HandlePhone=()=>{
+        Linking.openURL(`tel:03074170669`)
+    }
+    const HandleMap=()=>{
+        const lat = "2393.2";
+        const lng = "23333.2";
+        //openGps(lat,lng);
 
+        var scheme = Platform.OS === "ios" ? "maps:" : "geo:";
+        var url = scheme + `${lat},${lng}`;
+        Linking.openURL(url);
+
+    }
+  return (
+    <View style={styles.container}>
+      <View style={styles.innerContainer}>
+        <View style={styles.OpeningTimings}>
+          <Text style={styles.TextHeading}>
+            OpeningTimings: From 12 am to 6Pm
+          </Text>
         </View>
-        <View style={styles.backgroundMapHeader}>
-            <View style={styles.grid}></View>
-            {/* <View style={{flex: 0.2,}}> <ListItemSeparator/></View> */}
-           
-            <View style={styles.grid}></View>
-           
+        <View style={styles.PhoneNumber}>
+          <Text style={styles.PhoneHeadingText}>042-83483949384</Text>
         </View>
+        <View style={styles.Address}>
+          <Text style={styles.AddressText}>Food Court,Pakages Mall,LAHORE</Text>
         </View>
-    );
+      </View>
+      <View style={styles.backgroundMapHeader}>
+        <View style={styles.grid}>
+          <TouchableWithoutFeedback onPress={HandlePhone}>
+            <MaterialIcons name="phone" size={24} color="white" />
+          </TouchableWithoutFeedback>
+        </View>
+        <View style={{ flex: 0.2 }}></View>
+        <View style={styles.grid}>
+          <TouchableWithoutFeedback onPress={HandleMap}>
+            <MaterialIcons name="location-on" size={24} color="white" />
+          </TouchableWithoutFeedback>
+        </View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        marginTop:40,
-        
-    },
-    innerContainer:{
-        marginLeft:10,
-    },
-    OpeningTimings:{
-        
-    },
-    PhoneNumber:{
-        marginTop:4
-
-    },
-    Address:{},
-    AddressText:{
+  container: {
+    flex: 1,
+    marginTop: 40,
+  },
+  innerContainer: {
+    marginLeft: 10,
+  },
+  OpeningTimings: {},
+  PhoneNumber: {
+    marginTop: 4,
+  },
+  Address: {},
+  AddressText: {
     color: Colors.grey,
     fontSize: 11,
     fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-    width:"100%"
-    },
-    PhoneHeadingText:{
+    width: "100%",
+  },
+  PhoneHeadingText: {
     color: Colors.dark,
     fontSize: 12,
     fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-    width:"100%"
-    },
-    TextHeading:{
+    width: "100%",
+  },
+  TextHeading: {
     color: Colors.primary,
     fontSize: 18,
     fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-    width:"100%"
-    },
-    backgroundMapHeader:{
-        width:"100%",
-        height:40,
-        backgroundColor:Colors.medium,
-        flexDirection:"row"
-
-    },
-    grid:{
-        flex: 0.4,
-      //  backgroundColor:"white"
-    }
-
-
-
-    
-})
+    width: "100%",
+  },
+  backgroundMapHeader: {
+    width: "100%",
+    height: 40,
+    backgroundColor: Colors.medium,
+    flexDirection: "row",
+  },
+  grid: {
+    flex: 0.4,
+    //  backgroundColor:"white"
+    alignItems: "center",
+    alignSelf: "center",
+  },
+});
 export default LocationScreen;
